729546f507c68fe905b12bdbc423b78e63105ea6
Microseversk <alexander.osipov.work@gmail.com>
2025-03-04 02:03:59 +0700

    fix proxy

--- src/gateway/gateway.controller.ts
+++ src/gateway/gateway.controller.ts
@@ -1,34 +1,42 @@
 import { All, Controller, Req, Res } from '@nestjs/common';
 import { Request, Response } from 'express';
-import { createProxyMiddleware } from 'http-proxy-middleware';
+import { createProxyMiddleware, RequestHandler } from 'http-proxy-middleware';
 
 @Controller('gateway')
 export class GatewayController {
   private proxies = {
-    core: `http://${process}:3000`,
+    core: `http://localhost:3000`,
     users: 'http://localhost:3001',
   };
 
-  @All('*')
-  async proxy(@Req() req: Request, @Res() res: Response) {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const [_, serviceName, ...restPath] = req.path.split('/');
+  private proxyMiddlewares: Record<string, RequestHandler>;
 
-    const target = this.proxies[serviceName] as string;
-    if (!target) {
+  constructor() {
+    this.proxyMiddlewares = {};
+    Object.entries(this.proxies).forEach(([service, target]) => {
+      this.proxyMiddlewares[service] = createProxyMiddleware({
+        target,
+        changeOrigin: true,
+        pathRewrite: {
+          [`^/gateway/${service}`]: '',
+        },
+      });
+    });
+  }
+
+  @All('*')
+  proxy(@Req() req: Request, @Res() res: Response) {
+    const parts = req.path.split('/').filter(Boolean);
+    const serviceName = parts[1];
+
+    const proxyMiddleware = this.proxyMiddlewares[serviceName];
+    if (!proxyMiddleware) {
       return res.status(404).json({ message: 'Service not found' });
     }
 
-    console.log(`Proxying request to: ${target}/${restPath.join('/')}`);
-
-    const proxy = createProxyMiddleware({
-      target: target,
-      changeOrigin: true,
-      pathRewrite: {
-        [`^/${serviceName}`]: '',
-      },
-    });
-
-    return proxy(req, res, () => {});
+    console.log(
+      `Proxying request to service: ${serviceName}, path: ${req.path}`,
+    );
+    return proxyMiddleware(req, res, () => {});
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 1337);
+  await app.listen(process.env.PORT ?? 4000);
+
+  console.log(`Gateway is running on port ${process.env.PORT ?? 4000}`);
 }
 bootstrap();
