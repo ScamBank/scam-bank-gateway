5c0812baaec1d5eb52acf430374062b30e3d3dc8
Microseversk <alexander.osipov.work@gmail.com>
2025-03-08 19:00:35 +0700

    fix proxy

--- src/main.ts
+++ src/main.ts
@@ -11,11 +11,13 @@ async function bootstrap() {
   const proxies = {
     core: `http://${host}:3000`,
     users: `http://${host}:3001`,
+    users1c: 'https://cis.tsu.ru',
   };
   console.log('@proxies', proxies);
 
   app.use('/api/users', proxy(proxies.users));
   app.use('/api/core', proxy(proxies.core));
+  app.use('/api/users1c', proxy(proxies.users1c));
 
   await app.listen(process.env.PORT ?? 4000);
   console.log(`Gateway is running on port ${process.env.PORT ?? 4000}`);
