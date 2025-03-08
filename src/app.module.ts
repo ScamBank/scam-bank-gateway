13aca0fa40c805b778163949c5354ce5617dc37a
Microseversk <alexander.osipov.work@gmail.com>
2025-03-08 13:36:21 +0700

    update gateway

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { GatewayModule } from './gateway/gateway.module';
 
 @Module({
-  imports: [GatewayModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as proxy from 'express-http-proxy';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: {
-      origin: true,
-      credentials: true,
-    },
-  });
-  app.setGlobalPrefix('gateway');
-  await app.listen(process.env.PORT ?? 4000);
+  const app = await NestFactory.create(AppModule);
 
+  const host =
+    process.env.EXEC_ENV === 'production' ? '45.130.146.135' : 'localhost';
+
+  const proxies = {
+    core: `http://${host}:3000`,
+    users: `http://${host}:3001`,
+  };
+  console.log('@proxies', proxies);
+
+  app.use('/api/users', proxy(proxies.users));
+  app.use('/api/core', proxy(proxies.core));
+
+  await app.listen(process.env.PORT ?? 4000);
   console.log(`Gateway is running on port ${process.env.PORT ?? 4000}`);
 }
 bootstrap();
